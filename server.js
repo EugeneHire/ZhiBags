8bb8b24bfbca96b59cb67c844818ca24a104f4af
Alex Korich <alex.korichkovskiy@gmail.com>
2015-05-24 01:53:47 +0300

    delete works

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 fs = require('fs');
-var ara =require('./bags.json') ;
-
+var bagsJson =require('./bags.json') ;
+var bags= Object.keys(bagsJson).map(function(k) { return bagsJson[k] });
 
 
 app.get('/', function(req, res){
@@ -13,15 +13,23 @@ app.use('/js', express.static(__dirname + '/js'));
 
 
 app.get('/bags', function(req, res){ 
-	
- 	res.send(JSON.stringify(ara));
-    });
+	res.send(JSON.stringify(bags));
+});
 
 app.delete('/removebag:id', function(req, res){
-	console.log(req.params.id);
-
-
-	res.send(req.params.id);
+	id=req.params.id.shift()
+	bags = bags.filter(function(obj) {
+	return obj.id != id;
+	});
+	console.log(bags)
+	res.send(bags);
+	
+	fs.writeFile('bags.json', JSON.stringify(bags), function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(data);
+});
 });
 
 
