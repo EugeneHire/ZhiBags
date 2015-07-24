5f7496e0795851bae7f8a9fddb8d53e7019c0037
Alexey Korichkovskiy <johniezigger@gmail.com>
2015-07-24 18:09:44 +0300

    bags view attempt

--- client/js/BagView.js
+++ client/js/BagView.js
@@ -1,7 +1,13 @@
 var BagView=Backbone.View.extend({
-
+	template:_.template('<div>Available:<%= isAvalaible ? yes:no%></div>')
 	render:function(){
 		var html="<div>"+"<div>"+"Home"+"</div>"+"</div>";
-		$(this.el).html(html);
+		this.$el.html(this.template(attributes)); 
+	},
+	,
+	events: {
+		"click #add":"show_basket"
 	}
-})
+
+}
+ 
